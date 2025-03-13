7ffd7f7421d1da3561e234fb089042cd99bfeaf8
EvgenSavenok <eugen.savenok@gmail.com>
2025-03-13 10:40:28 +0300

    Feat: some models for SignalR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TaskComponent} from './components/task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TaskManager';
+  title = 'TaskManagerFrontend';
 }
---
+++ src/app/components/task.component.ts
@@ -0,0 +1,41 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { TaskDto } from '../models/task.model';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  imports: [
+    NgForOf
+  ],
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+  tasks: TaskDto[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.addTaskUpdateListener();
+  }
+
+  updateTask(): void {
+    const updatedTask: TaskDto = {
+      TaskId: 'some-guid',
+      UserId: 'user-id',
+      Title: 'Updated Task',
+      Description: 'Updated task description',
+      Category: 1,
+      Priority: 1,
+      Deadline: new Date(),
+      MinutesBeforeDeadline: 30,
+      UserTimeZone: 'UTC',
+      UserEmail: 'user@example.com',
+      TaskTags: [],
+      TaskComments: []
+    };
+
+    this.taskService.sendTaskUpdate(updatedTask);
+  }
+}
