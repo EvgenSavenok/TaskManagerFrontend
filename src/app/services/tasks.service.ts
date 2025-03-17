2456f41e9b344b8e29d41d8b1a2f650bf72ed566
EvgenSavenok <eugen.savenok@gmail.com>
2025-03-17 20:26:53 +0300

    Revert "Feat: users module"
    This reverts commit e914b23b8fc18e881e00d0500d8c1682c4c4aba6.

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,7 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { TaskDto } from '../moduls/task.model';
+import { TaskDto } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root'
